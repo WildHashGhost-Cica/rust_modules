c6c70907d655fd3505e199150bc1fb83cd1ac775
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-05 11:46:20 +0100

    modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+
 pub mod a{
     pub mod b{
         pub mod c{
@@ -7,14 +9,25 @@ pub mod a{
         }
     }
 }
+enum Ex{
+        A,
+        B,
+        C,
+}
 
 use a::b::c::d;
+use Ex::{A,C};
+//or
+use Ex::*;
 
 fn main() {
     //add e function different way
     //a::b::c::d::e();
 
     d::e();
+    Ex::B;
+    A; //use Ex::{A,C}
+    C; //use Ex::{A,C}
 
 
 }
